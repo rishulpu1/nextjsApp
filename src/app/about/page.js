7a80bc4fbfd9dc61c45b89ee6e7e15b04720ecba
Rajneesh Kumar <rishulpu1@gmail.com>
2023-07-10 17:23:41 +0530

    added new pages

---
+++ src/app/about/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const About = ()=>{
+    return(
+        <>
+        
+        <h1>About Page</h1>
+        <Link href="/">Return to Home Page</Link>
+        </>
+    );
+}
+
+export default About;
---
+++ src/app/news/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const News = ()=> {
+    return (
+        <>
+            <h1>News Page</h1>
+            <Link href="/">Return to Home Page</Link>
+        </>
+        
+    );
+}
+
+export default News;
