9d2ed5fb3ef39133968526d0fd3b34dc08b1a575
Rajneesh Kumar <rishulpu1@gmail.com>
2023-07-13 23:26:55 +0530

    updated meetup component

--- src/app/new-meetup/page.js
+++ src/app/new-meetup/page.js
@@ -1,8 +1,14 @@
+"use client"
+import NewMeetupForm from "../../../components/meetups/NewMeetupForm";
+
 function NewMeetup(){
-    
+    function onAddMeetupHandler(addedMeetupData){
+        console.log(addedMeetupData);
+    }
     return(
         <>
-       <h1>Create New Meetup</h1>
+            <h1>Create New Meetup</h1>
+            <NewMeetupForm onAddMeetup={onAddMeetupHandler} />
         </>
     );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,26 @@
-import Link from 'next/link'
+import MeetupList from "../../components/meetups/MeetupList"
 
 export default function Home() {
+  const DUMMY_MEETUPS = [
+    {
+      id: 'e1',
+      title: 'First Meetup',
+      image: 'https://rajneeshk.in/images/projects/eascost/thumb.jpg',
+      address: 'Fl 5C Panorama Floor',
+      description: 'This is first meetup'
+    },
+    {
+      id: 'e2',
+      title: 'Second Meetup',
+      image: 'https://rajneeshk.in/images/projects/greeninvestment/Green-Investment.jpg',
+      address: 'Fl 6C Panorama Floor',
+      description: 'This is second meetup'
+    }
+  ];
   return (
     <div>
-      <h1>Home Page</h1>
-      <Link href="/about">About Us</Link>
-      <Link href="/news">News</Link>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={DUMMY_MEETUPS} />
     </div>
     
   )
