db30c23529d32a8c0e9e5e1a725651640477ed10
Rajneesh Kumar <rishulpu1@gmail.com>
2023-07-16 00:22:06 +0530

    added dynamic navigation and meetup detail page

---
+++ src/app/[meetupid]/page.js
@@ -0,0 +1,13 @@
+import MeetupDetail from "../../../components/meetups/MeetupDetail";
+const MeetupDetails = () => {
+    return (
+        <MeetupDetail 
+            image = "https://rajneeshk.in/images/projects/eascost/thumb.jpg"
+            title = "First Meetup"
+            address = "Fl 5C Panorama Floor"
+            description = "Lorem ipsum first meetup lorem ipsum"
+        />
+    )
+}
+
+export default MeetupDetails;
